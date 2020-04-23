144c767a2f46493bb1708d7e122da24715a25a3c
Pierre <pierre74d@gmail.com>
2020-04-23 18:35:37 +0200

    Remove unused code

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {createStackNavigator} from "@react-navigation/stack";
 import {StatusBar, View} from "react-native";
 import Navigator from "./src/navigations/homeStack";
 
